f5ea865fa661f7a2372ef2b8fcfbeb45136c3c2b
suleib123 <107375496+suleib123@users.noreply.github.com>
2023-03-28 14:43:17 +0000

    additional a7 work

--- a7.py
+++ a7.py
@@ -2,5 +2,5 @@ from neural import *
 
 # Question 1 Below
 net_1 = NeuralNet(2, 2, 1)
-data = [(0, 0), (1, 1), (1, 1), (2, 0)]
+data = [(0, 0), (1, 1), (1, 1), (2, 0)] # NOTE: Inputs(first value of tuple) needs to be a list
 net_1.train(data)
